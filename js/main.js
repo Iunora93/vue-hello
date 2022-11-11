63c3a46cea89c072896100216d8d88ec6962f459
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-11 15:44:05 +0100

    Inserita immagine

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            textImage: {
+                message: 'Hello Vue!',
+                image: 'https://www.positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png'
+            }
         }
     }
 }).mount('#app')
