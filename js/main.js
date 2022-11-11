ffff09666a79f68d31e097280c6ba88a168fbefe
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-11 16:19:37 +0100

    Aggiunte classi css

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ createApp({
         return {
             textImage: {
                 message: 'Hello Vue!',
-                image: 'https://www.positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png'
+                image: 'https://www.positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png',
+                cssClassList: "size",
+                center: "center"
             }
         }
     }
